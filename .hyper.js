60a6a654e66974da6fd82fc8635e9b1be7da0a65
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2016-12-17 02:45:38 +0100

    Use vim as default editor

--- .hyper.js
+++ .hyper.js
@@ -7,10 +7,17 @@ module.exports = {
     foregroundColor: '#fff',
     backgroundColor: 'rgba(0, 0, 0, 1)',
     borderColor: '#333',
-    css: '',
+    css: `
+      .term_fit:not(.term_term) {opacity: .6;}
+      .term_fit.term_active {
+        opacity: 1;
+        transition: opacity 0.12s ease-in-out;
+        will-change: opacity;
+      }
+    `,
     termCSS: `
-    x-screen a {color: rgba(252, 60, 131, 1);}
-    x-screen a.hover {text-decoration: underline;}
+      x-screen a {color: rgba(252, 60, 131, 1);}
+      x-screen a.hover {text-decoration: underline;}
     `,
     padding: '12px 14px',
     colors: {
