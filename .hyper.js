6c5fa60a9fa11989cf0e7d777d3816de6d09d025
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2017-12-08 10:00:02 +0100

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -1,14 +1,18 @@
-// for advanced config flags please refer to https://hyper.is/#cfg
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
 module.exports = {
   config: {
+    updateChannel: 'stable',
     fontSize: 15,
-    fontFamily: '"Roboto Mono for Powerline", "Lucida Console", monospace',
-    cursorColor: 'rgba(248,28,229,0.8)', // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    fontFamily: '"Fira Mono for Powerline", "Lucida Console", monospace',
+    cursorColor: 'rgba(248,28,229,0.8)',
     cursorShape: 'BLOCK', // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorBlink: true,
-    foregroundColor: '#fff', // color of the text
-    backgroundColor: '#000', // terminal background color
-    borderColor: '#333', // border color (window, tabs)
+    cursorBlink: false,
+    foregroundColor: '#fff',
+    backgroundColor: '#282A36',
+    borderColor: '#333',
     css: `
       .term_fit:not(.term_term) { opacity: .6; }
       .term_fit.term_active {
@@ -21,64 +25,53 @@ module.exports = {
       x-screen a { color: rgba(252, 60, 131, 1); }
       x-screen a.hover { text-decoration: underline; }
     `,
-    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
-    // default: `false` on Linux, `true` on Windows (ignored on macOS)
-    showHamburgerMenu: '',
-    // set to `false` if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` on windows and Linux (ignored on macOS)
-    showWindowControls: '',
     padding: '12px 14px',
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
     colors: {
-      black: 'rgba(46, 46, 46, 1)',
-      red: 'rgba(236, 35, 66, 1)',
-      green: 'rgba(130, 188, 51, 1)',
-      yellow: 'rgba(255, 195, 45, 1)',
-      blue: 'rgba(52, 100, 163, 1)',
-      magenta: 'rgba(229, 23, 113, 1)',
-      cyan: 'rgba(25, 163, 162, 1)',
-      white: 'rgba(248, 249, 254, 1)',
-      lightBlack: 'rgba(87, 75, 79, 1)',
-      lightRed: 'rgba(210, 55, 75, 1)',
-      lightGreen: 'rgba(138, 215, 177, 1)',
-      lightYellow: 'rgba(254, 217, 105, 1)',
-      lightBlue: 'rgba(29, 174, 237, 1)',
-      lightMagenta: 'rgba(254, 188, 208, 1)',
-      lightCyan: 'rgba(128, 235, 252, 1)',
-      lightWhite: 'rgba(243, 243, 243, 1)'
+      black: '#000000',
+      red: '#ff0000',
+      green: '#33ff00',
+      yellow: '#ffff00',
+      blue: '#0066ff',
+      magenta: '#cc00ff',
+      cyan: '#00ffff',
+      white: '#d0d0d0',
+      lightBlack: '#808080',
+      lightRed: '#ff0000',
+      lightGreen: '#33ff00',
+      lightYellow: '#ffff00',
+      lightBlue: '#0066ff',
+      lightMagenta: '#cc00ff',
+      lightCyan: '#00ffff',
+      lightWhite: '#ffffff'
     },
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    // make sure to use a full path if the binary name doesn't work
-    // (e.g `C:\\Windows\\System32\\bash.exe` instead of just `bash.exe`)
-    // if you're using powershell, make sure to remove the `--login` below
     shell: '/usr/local/bin/bash',
-    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-    // by default ['--login'] will be used
     shellArgs: ['--login'],
-    env: {}, // for environment variables
-    bell: 'SOUND', // set to false for no bell
-    copyOnSelect: false // if true, selected text will automatically be copied to the clipboard
-    // if true, on right click selected text will be copied or pasted if no
-    // selection is present (true by default on Windows)
-    // quickEdit: true
-    // bellSoundURL: 'http://example.com/bell.mp3', // URL to custom bell
+    env: {},
+    bell: 'SOUND',
+    copyOnSelect: false,
+    hyperTabs: {
+      trafficButtons: true,
+      border: true,
+      tabIconsColored: true,
+      activityColor: "#ED8492",
+      closeAlign: 'right',
+    }
+    // bellSoundURL: 'http://example.com/bell.mp3',
   },
   plugins: [
-    'hyperlinks',
-    'hypercwd',
+    "hypercwd",
+    "hyper-tabs-enhanced",
+    "hyper-chesterish",
     "hyper-statusline",
-    "hyperlayout",
     "hyperterm-alternatescroll",
-    "hyper-tab-icons",
-    "hyper-simple-vibrancy",
-    "hyper-snazzy",
-    "hyper-search",
+    "hyper-confirm",
     "hyper-quit",
-    "hyper-confirm"
+    "hyperlinks",
+    "hyper-snazzy"
   ],
-  localPlugins: []
+  localPlugins: [],
+  keymaps: {
+    // Example
+    // 'window:devtools': 'cmd+alt+o',
+  }
 };
